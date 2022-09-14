149ffb86a64e3325d7d82a8530f6fa28fa6a2153
Elmo <elmo@clo3d.com>
2022-09-15 07:11:30 +0900

    [feat] add solve

--- source/biharmonic_precompute.cpp
+++ source/biharmonic_precompute.cpp
@@ -4,12 +4,28 @@
 #include <igl/massmatrix.h>
 #include <igl/invert_diag.h>
 
-void biharmonic_precompute(
-  const Eigen::MatrixXd & V,
-  const Eigen::MatrixXi & F,
-  const Eigen::VectorXi & b,
-  igl::min_quad_with_fixed_data<double> & data)
+void biharmonic_precompute(const Eigen::MatrixXd & V,const Eigen::MatrixXi & F,const Eigen::VectorXi & b,igl::min_quad_with_fixed_data<double> & data)
 {
   // REPLACE WITH YOUR CODE
-  data.n = V.rows();
+  // data.n = V.rows();
+
+    // Construct mass and Laplacian Matrices
+    Eigen::SparseMatrix<double> L;
+    Eigen::SparseMatrix<double> M;
+    igl::massmatrix(V, F, igl::MassMatrixType::MASSMATRIX_TYPE_BARYCENTRIC, M);
+    igl::cotmatrix(V, F, L);
+    
+    // Compute M^{-1}
+    Eigen::SparseMatrix<double> I(V.rows(), V.rows());
+    I.setIdentity();
+    Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> ldlt(M);
+    Eigen::SparseMatrix<double> inv_M = ldlt.solve(M); 
+  
+    // Calculate Q = L^T * M^{-1} * L = L * M^{-1} * L;
+    Eigen::SparseMatrix<double> Q = L * inv_M * L; 
+  
+    // Precompute decomposition for Q in data structure.
+    Eigen::SparseMatrix<double> A;
+    A.setZero(); 
+    igl::min_quad_with_fixed_precompute(Q, b, A, false, data);
 }
